ac43f058470a906e6f46a3f7b36e08ca23d9de91
Gaurav Bhardwaj <gau.bwj@gmail.com>
2026-04-08 01:07:48 +0530

    display checkout button when product is in the cart otherwise show add to cart button

--- src/Components/ProductDetail.jsx
+++ src/Components/ProductDetail.jsx
@@ -1,41 +1,64 @@
-import { useParams } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useParams, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { addtocart } from "../utils/Cart/Cartslice";
 import Data from "../utils/Data";
-import './Productdetail.css'
+import "./Productdetail.css";
 
-function ProductDetail(){
-    const {id}=useParams();
-    let products=Data();
-    const product=products.filter(p=>p.id==id);
-    const dispatch=useDispatch()
-    
-    function handleaddTocart(item){
-      dispatch(addtocart(item));
-    }
-    
-    return(<>
-    {
-        product.map((p)=>{
-            return(
-            <div key={p.id} className="productdetail">
-                <div className="productimages">
-                  <img src={p.images} alt="" loading="lazy" />
-                </div>
-                <div className="productinfo">
-               <h2 className="producttitle">{p.title}</h2>
-               <p className="productdescription">{p.description}</p>
-               <p>{p.price}<span>$</span></p>
-               <button className="addtocart" onClick={()=>handleaddTocart(p)}>Add to cart</button>
-                </div>
-                
+function ProductDetail() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const products = Data();
+  const product = products.filter(p => p.id == id);
+
+  const cartItems = useSelector(state => state.cart.list);
+
+  function handleaddTocart(item) {
+    dispatch(addtocart(item));
+  }
+
+  return (
+    <>
+      {product.map((p) => {
+        // check if product is already in cart
+        const isInCart = cartItems.some(item => item.id == p.id);
+
+        return (
+          <div key={p.id} className="productdetail">
+            <div className="productimages">
+              <img src={p.images} alt={p.title} loading="lazy" />
             </div>
-           
-            )
-        })
-    }
 
-    
-    </>);
+            <div className="productinfo">
+              <h2 className="producttitle">{p.title}</h2>
+              <p className="productdescription">{p.description}</p>
+              <p>
+                {p.price} <span>$</span>
+              </p>
+
+              {/* Conditional Button */}
+              {isInCart ? (
+                <button
+                  className="checkout"
+                  onClick={() => navigate("/checkout")}
+                >
+                  Go to Checkout
+                </button>
+              ) : (
+                <button
+                  className="addtocart"
+                  onClick={() => handleaddTocart(p)}
+                >
+                  Add to Cart
+                </button>
+              )}
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
 }
+
 export default ProductDetail;
