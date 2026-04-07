ef01673857f21fa4e9cbc2c8cb61b69e359436f3
Gaurav Bhardwaj <gau.bwj@gmail.com>
2026-04-07 23:58:40 +0530

    imported footer to layout component and rendering it

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,10 +1,11 @@
 import Header from "./Header"
+import Footer from "./Footer";
 import { Outlet } from "react-router-dom"
 function Layout(){
     return(<>
     <Header/>
     <Outlet/>
-    
+    <footer/>
     </>)
 }
 
